509d7ad59793ea5de9ea0639801645629f8c9cfd
changyuqing <changyuqing_@outlook.com>
2019-05-05 19:34:37 +0800

    [add] 交换列表中的相邻节点

---
+++ src/com/_SwapPairs/MainClass.java
@@ -0,0 +1,93 @@
+package com._SwapPairs;
+
+// https://leetcode.com/problems/swap-nodes-in-pairs
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import com.util.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null||head.next == null){
+            return head;
+        }
+        ListNode newHead = head.next;
+        ListNode temp = head;
+        ListNode tempHead = new ListNode(0);
+        while (temp!=null&&temp.next!=null){
+            tempHead.next = temp.next;
+            temp.next = tempHead.next.next;
+            tempHead.next.next = temp;
+            tempHead = temp;
+            temp = temp.next;
+        }
+        return newHead;
+    }
+}
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+
+            ListNode ret = new Solution().swapPairs(head);
+
+            String out = listNodeToString(ret);
+
+            System.out.print(out);
+        }
+    }
+}
