92be118b37745b617e912098552beedc66ac0a41
changyuqing <changyuqing_@outlook.com>
2019-06-24 18:29:42 +0800

    [add] 移除有序数组中的重复元素 [add] 移除数组中的指定元素

---
+++ src/com/_RemoveDuplicates/MainClass.java
@@ -0,0 +1,75 @@
+package com._RemoveDuplicates;
+
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+        int k = 1;
+        for (int i = 0; i + k < nums.length; i++) {
+            while (i + k < nums.length && nums[i] == nums[i + k]) {
+                k++;
+            }
+            if (i + k < nums.length) {
+                nums[i + 1] = nums[i + k];
+            }
+
+        }
+        return nums.length - k + 1;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+
+        String result = "";
+        for (int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+
+            int ret = new Solution().removeDuplicates(nums);
+            String out = integerArrayToString(nums, ret);
+
+            System.out.print(out);
+        }
+    }
+}
